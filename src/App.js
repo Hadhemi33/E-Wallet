6fe65947f71dfe8cf8548e8e99189efd65b4abee
Hadhemi33 <hadhemi.benmansour@isimg.tn>
2023-06-05 06:07:06 +0100

    Transaction done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import AccountCreate from "./scenes/AccountCreate.tsx";
+
+function App() {
+  return (
+    <div class="App-header">
+      <div className="App m-3">
+        <AccountCreate  className="account"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/TransactionService.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { goerli, sepolia } from "../models/Chain.ts";
+
+export class TransactionService {
+  static API_URL = "https://deep-index.moralis.io/api/v2";
+  static API_KEY =
+    "xcrD2RZ8X0N8Y5bWBydGVfeDd0j7KOndWEp7LKVo2s00TrRt5AN4Zvta2MScRMBr";
+  // "EuwYtjWwWHGbnwsCnGauMtMMaEQZugtjaws2ybm2ZpSR15a8vzl6QUPkEUWHTOCU";
+
+  static async getTransactions(address: string) {
+    const options = {
+      method: "GET",
+      url: `${TransactionService.API_URL}/${address}`,
+      params: { chain: sepolia.name.toLowerCase() },
+      headers: {
+        accept: "application/json",
+        "X-API-Key": TransactionService.API_KEY,
+      },
+    };
+
+    const response = await axios.request(options);
+    return response;
+  }
+}
